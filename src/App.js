c6d93d81fb08955d89e1c2d2791d22033aa2eba7
Jaishnav <jaishnavgopi@gmail.com>
2021-06-05 10:47:32 +0530

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { NavHashLink as Link } from 'react-router-hash-link';
 
 import Home from './components/homeComponent.js';
